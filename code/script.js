854291ce25e13ed731b9c9ce30e2893bca91cea4
V-Ivanich <chipai_dom@mail.ru>
2022-01-28 22:06:12 +0300

    polychilos

--- code/script.js
+++ code/script.js
@@ -3,11 +3,12 @@ const btnLeft = document.querySelector('#lev'),
   btnRight = document.querySelector('#prav');
 
 let pointY = [-105, 0, 105, 210, 315],
+  conteiner = document.querySelector('#slide'),
   slides = document.querySelectorAll('.slide-single'),
-  temporary ,//временный массив
+  firChild,
   slider = [];
 
-  //скопировали в массив и удалили из html
+//скопировали в массив и удалили из html
 for (let i = 0; i < slides.length; i++) {
   slider[i] = slides[i].src;
   slides[i].remove();
@@ -17,15 +18,22 @@ let indexL = 0,
   indexR = 4,
   flag = 0,
   raschet,
+  check,
   massivItems;
 
 //фун-я создания и инициализации элемента
-function bornItem(step, offset) {
+function bornItem(step, offset, check) {
   let img = document.createElement('img');
+
   img.src = slider[step];
   img.classList.add('slide-single');
   img.style.left = pointY[offset] + 'px';
   document.querySelector('#slide').appendChild(img);
+
+  if (check == 1) {
+    firChild = conteiner.firstChild;
+    conteiner.insertBefore(img, firChild);
+  }
 }
 
 //начальная установка элементов
@@ -35,35 +43,30 @@ for (let i = 0; i != 5; i++) {
   if (i == 4) {
     break;
   }
-  
+
 }
 
 //прокрутка слайдера в обе стороны
 // в зависимости от флага
 function effectSlide(flag) {
   massivItems = document.querySelectorAll('.slide-single');
-  temporary = document.querySelectorAll('.slide-single');
-  massivItems = Array.from(massivItems);
+
   if (flag == 0) {
-    massivItems.shift();//1-й элемент удаляем
-    temporary[0].remove();
+    massivItems[0].remove();//1-й элемент удаляем
     for (let i = 1; i < massivItems.length; i++) {
       massivItems[i].style.left = pointY[i - 1] + 'px';
     }
     bornItem(indexR, 4);
-    massivItems.push(temporary[temporary.length - 1]);    
-    console.log(massivItems);
+
   }
 
   if (flag == 1) {
-    temporary.pop();//последний элемент удаляем
+    //последний элемент удаляем
     massivItems[4].remove();
     for (let k = massivItems.length - 1; k > 0; k--) {
       massivItems[k - 1].style.left = pointY[k] + 'px';
     }
-    bornItem(indexL, 0);
-    temporary.unshift(slider[indexL]);
-    console.log(temporary);
+    bornItem(indexL, 0, 1);
   }
 }
 
