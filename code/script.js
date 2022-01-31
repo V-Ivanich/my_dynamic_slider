407cc2e26d3604e1b979c02e709d2d09813fee6f
V-Ivanich <chipai_dom@mail.ru>
2022-01-31 20:07:45 +0300

    new logic

--- code/script.js
+++ code/script.js
@@ -10,6 +10,8 @@ let pointY = [-105, 0, 105, 210, 315],//координаты вывода кар
   conteiner = document.querySelector('#slide'),
   slides = document.querySelectorAll('.slide-single'),
   firChild,
+  minus,
+  plus,
   slider = [];
 
 //скопировали в массив и удалили из html
@@ -93,7 +95,7 @@ btnLeft.addEventListener('click', leftButton);
 
 //правая кнопка
 //и расчет первой и последней позиции
-function rightButton (){
+function rightButton() {
   indexR++;
   if (indexR > 8) {
     indexR = 0;
@@ -119,24 +121,40 @@ function spanClear() {
 span1.addEventListener('click', () => {
   spanClear();
   let cikl = positionSpan(1)
-      for(let i = 0; i < cikl ; i++){
-        if(flag ==1){
-          leftButton();
-        }
-        else {
-          rightButton();
-        }
-      }
+  for (let i = 0; i < cikl; i++) {
+    if (flag == 1) {
+      setTimeout(leftButton, 100);
+    }
+    else {
+      setTimeout(rightButton, 100);
+    }
+  }
 })
 
 span2.addEventListener('click', () => {
   spanClear();
-  span2.classList.add('on');
+  let cikl = positionSpan(2)
+  for (let i = 0; i < cikl; i++) {
+    if (flag == 1) {
+      setTimeout(leftButton, 100);
+    }
+    else {
+      setTimeout(rightButton, 100);
+    }
+  }
 })
 
 span3.addEventListener('click', () => {
   spanClear();
-  span3.classList.add('on');
+  let cikl = positionSpan(3)
+  for (let i = 0; i < cikl; i++) {
+    if (flag == 1) {
+      setTimeout(leftButton, 100);
+    }
+    else {
+      setTimeout(rightButton, 100);
+    }
+  }
 })
 
 function spanActive() {
@@ -145,7 +163,7 @@ function spanActive() {
       spanClear();
       span1.classList.add('on');
       break;
-      case 3:
+    case 3:
       spanClear();
       span2.classList.add('on');
     case 6:
@@ -155,19 +173,76 @@ function spanActive() {
   }
 }
 
-function positionSpan(n){
-  let inL = indexL;
+function positionSpan(n) {
+  let inL = indexL,
+    x;
+  x = inL;
+  plus = 0; minus = 0;
 
-  if(n == 1){
-    if(inL == 0) return;
-    inL -=8;
-    inL *= -1;
-    if(inL <= 4){
-      flag =0;
+  if (n == 1) {
+    if (inL == 0) return;
+    while (x != 0) {
+      x--;
+      minus++;
     }
-    if(inL > 4) {
-      flag =1; 
-    } 
-    return inL;
+    while (inL != 9) {
+      inL++;
+      plus++;
+    }
+    if (minus > plus) {
+      flag = 0;
+      return plus;
+    }
+    else {
+      flag = 1;
+      return minus;
+    }
+  }
+
+  if (n == 2) {
+    if (inL == 3) return;
+    while (x > 3 && x < 9) {
+      x--;
+      minus++;
+    }
+    if (inL == 8) inL = -1;
+    while (inL != 3) {
+      inL++;
+      plus++;
+    }
+    if (minus > plus) {
+      flag = 0;
+      return plus;
+    }
+    else {
+      flag = 1;
+      return minus;
+    }
+  }
+
+  if (n == 3) {
+    if (inL == 6) return;
+    if (x == 0) x = 9;
+    if (x == 1) x = 10;
+    if (x > 6) {
+      while (x != 6) {
+        x--;
+        minus++;
+      }
+    }
+    if (inL < 6) {
+      while (inL != 6) {
+        inL++;
+        plus++;
+      }
+    }
+    if (minus > plus) {
+      flag = 0;
+      return plus;
+    }
+    else {
+      flag = 1;
+      return minus;
+    }
   }
 }
